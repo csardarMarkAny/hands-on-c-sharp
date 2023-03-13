f9eda10a98fff92e9bddccff8a8d99f6c3a58449
Chittaranjan Sardar <csardar@markany.com>
2023-03-13 07:11:50 +0530

    SF-332 - C# Hands On, A REST Webservice in C#.

---
+++ hello-world/HelloWorld/HelloWorld/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+public class MyClass
+{
+
+	static void Main(string[] args) {
+		Console.WriteLine("MyClass: Hello World!");
+		Console.WriteLine("Current Thread State = " + Thread.CurrentThread.ThreadState);
+        Console.WriteLine("Current Thread Name = " + Thread.CurrentThread.Name);
+
+		MyClass myClass = new MyClass();
+
+		Thread thread1 = new Thread(new ThreadStart(myClass.threadMethod));
+		thread1.Start();
+    }
+
+
+	public void threadMethod()
+	{
+		for (var i = 0; i < 10; i++) {
+
+			Console.WriteLine(i);
+		}
+
+	}
+}
